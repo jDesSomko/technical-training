6872bb1528d2e0e610768fe65f4688783fda721a
Jamy Destickere <jamy.destickere@student.howest.be>
2018-10-01 10:42:06 +0200

    Added dashboard action

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -1,4 +1,15 @@
-/**
- * Created by Jamy on 10/1/18.
- */
-console.log("Test")
+odoo.define('awesome_tshirt.dashboard', function (require) {
+    "use strict";
+
+    var ControlPanelMixin = require('web.ControlPanelMixin');
+    var AbstractAction = require('web.AbstractAction');
+    var core = require('web.core');
+
+    var ClientAction = AbstractAction.extend(ControlPanelMixin, {
+
+    });
+
+    core.action_registry.add('awesome-tshirt-action', ClientAction);
+
+    return {};
+});
