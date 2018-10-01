7d8e3eaca1f9742fcf5b75cf8035effec732fe68
Jamy Destickere <jamy.destickere@student.howest.be>
2018-10-01 13:39:36 +0200

    Updated dashboard view

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -9,7 +9,9 @@ odoo.define('awesome_tshirt.Dashboard', function (require) {
     var Dashboard = AbstractAction.extend(ControlPanelMixin, {
         template: 'awesome_tshirt.dashboard',
         events: {
-            'click button.customer': '_onClickCustomer'
+            'click button.customer': '_onClickCustomer',
+            'click button.cancelled_orders': '_onClickCancelledOrders',
+            'click button.new_orders': '_onClickNewOrders'
         },
         start: function () {
             var counter = new Counter(this, 4);
@@ -18,6 +20,24 @@ odoo.define('awesome_tshirt.Dashboard', function (require) {
             counter.appendTo(this.$el.find('.counter'));
         },
         _onClickCustomer: function (ev) {
+            ev.preventDefault();
+            return this.do_action({
+                type: 'ir.actions.act_window',
+                res_model: 'res.partner',
+                views: [[false, 'kanban']],
+                target: 'current'
+            });
+        },
+        _onClickNewOrders: function (ev) {
+            ev.preventDefault();
+            return this.do_action({
+                type: 'ir.actions.act_window',
+                res_model: 'res.partner',
+                views: [[false, 'kanban']],
+                target: 'current'
+            });
+        },
+        _onClickCancelledOrders: function (ev) {
             ev.preventDefault();
             return this.do_action({
                 type: 'ir.actions.act_window',
