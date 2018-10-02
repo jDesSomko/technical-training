08cf647a89e15bf24e1cc39976ec9fa8978760ff
Jamy Destickere <jamy.destickere@student.howest.be>
2018-10-02 14:59:19 +0200

    Requires

--- awesome_tshirt/static/src/js/quick_order_navigation.js
+++ awesome_tshirt/static/src/js/quick_order_navigation.js
@@ -2,6 +2,11 @@ odoo.define('awesome_tshirt.QuickOrderNavigation', function (require) {
     "use strict";
 
     var SystrayMenu = require('web.SystrayMenu');
+    var Widget = require('web.Widget');
+    var core = require('web.core');
+
+    var qweb = core.qweb;
+    var _t = core._t;
 
     var QuickOrderNavigation = Widget.extend({
 
