6c5c8a1ab39f941559a47e2d98e1a513d21945b0
Jamy Destickere <jamy.destickere@student.howest.be>
2018-10-02 13:54:32 +0200

    Added renderbutton

---
+++ awesome_tshirt/static/src/js/geolocate_form_view.js
@@ -0,0 +1,36 @@
+odoo.define('awesome_tshirt.geolocate_form_view', function (require) {
+    'use strict';
+
+    var viewRegistry = require('web.view_registry');
+
+    var AbstractField = require('web.AbstractField');
+    var core = require('web.core');
+
+    var FormView = require('web.FormView');
+    var FormController = require('web.FormController');
+
+    var basicFields = require('web.basic_fields');
+
+    var _t = core._t;
+
+    var GeoLocateFormController = FormController.extend({
+        renderButtons: function ($node) {
+            this._super.apply(this, arguments)
+
+            if (this.mode !== 'create') {
+                this.$buttons.append(qweb.render("awesome_tshirt.GeoLocateButton", {widget: this}));
+                this.$buttons.on('click', '.o_button_geolocate', this._onGeoLocate.bind(this));
+            }
+        }
+    });
+
+    var GeoLocateFormView = FormView.extend({
+        config: _.extend({}, FormView.prototype.config, {
+            Controller: GeoLocateFormController
+        })
+    });
+
+    viewRegistry.add('geolocate_form_view', GeoLocateFormView);
+
+    return FieldBooleanIndicator
+});
