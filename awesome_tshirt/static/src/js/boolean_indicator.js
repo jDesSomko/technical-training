35a27f41b27445a0dab7f1eb1983a0d97d1e653b
Jamy Destickere <jamy.destickere@student.howest.be>
2018-10-02 11:21:14 +0200

    boolean indicator

---
+++ awesome_tshirt/static/src/js/boolean_indicator.js
@@ -0,0 +1,37 @@
+odoo.define('awesome_tshirt.boolean_indicator', function (require) {
+    'use strict';
+
+    var AbstractField = require('web.AbstractField');
+    var core = require('web.core');
+    var field_registry = require('web.field_registry');
+
+    var basicFields = require('web.basic_fields');
+
+    var _t = core._t;
+
+    var FieldBooleanIndicator = basicFields.FieldBoolean.extend({
+        className: 'o_field_boolean_indicator',
+
+        //--------------------------------------------------------------------------
+        // Private
+        //--------------------------------------------------------------------------
+
+        /**
+         * In readonly, should be a img.
+         *
+         * @override
+         * @private
+         */
+        _renderReadonly: function () {
+            if (this.value) {
+                this.$el = $('<span class="boolean_indicator boolean_indicator_true" />')
+            } else {
+                this.$el = $('<span class="boolean_indicator boolean_indicator_false" />')
+            }
+        }
+    });
+
+    field_registry.add('boolean_indicator', FieldBooleanIndicator);
+
+    return FieldBooleanIndicator
+});
